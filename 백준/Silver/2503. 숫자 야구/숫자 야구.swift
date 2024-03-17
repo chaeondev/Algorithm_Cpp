3fee012f146819a875cc112233af9014dac90bc4
Chaewon Yoon <80023607+chaeondev@users.noreply.github.com>
2024-03-18 05:20:44 +0900

    [Silver III] Title: 숫자 야구, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.swift
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 let n = Int(readLine()!)!
 var hint: [[Int]] = []
 for _ in 0..<n {
@@ -5,66 +7,55 @@ for _ in 0..<n {
 }
 
 var answer = 0
-for a in 1..<10 { //100의 자리수
-    for b in 1..<10 { // 10의 자리수
-        for c in 1..<10 { // 1의 자리수
-            
-            if a == b || b == c || c == a { // 똑같은 숫자 있으면 제외
-                continue
-            }
-            //숫자가 정해졌다면
-            
-            var cnt = 0
-            for arr in hint {
-                let number = arr[0]
-                let strike = arr[1]
-                let ball = arr[2]
-                
-                //a,b,c 라는 숫자를 number와 비교해서
-                //자리수를 나눠서 strike ball을 측정하는 부분
-                var ballCnt = 0
-                var strikeCnt = 0
-                let numA = number / 100 // number의 100의 자리수
-                let numB = (number % 100) / 10 //number의 10의 자리수
-                let numC = number % 10 //number의 1의 자리수
-                
-                if [a,b,c].contains(numA) {
-                    if a == numA {
-                        strikeCnt += 1
-                    } else {
-                        ballCnt += 1
-                    }
-                }
-                
-                if [a,b,c].contains(numB) {
-                    if b == numB {
-                        strikeCnt += 1
-                    } else {
-                        ballCnt += 1
-                    }
-                }
-                
-                if [a,b,c].contains(numC) {
-                    if c == numC {
-                        strikeCnt += 1
-                    } else {
-                        ballCnt += 1
-                    }
-                }
-                
-                
-                if ball == ballCnt && strike == strikeCnt {
-                    cnt += 1
-                }
-                
-            }
-            
-            if cnt == n {
-                answer += 1
-            }
+for number in 123...987 {
+    let numA = number / 100
+    let numB = (number % 100) / 10
+    let numC = number % 10
+    
+    // 0을 포함하는 숫자 또는 중복된 숫자가 있는 경우 건너뜀
+    if numA == 0 || numB == 0 || numC == 0 || numA == numB || numB == numC || numC == numA {
+        continue
+    }
+    
+    var cnt = 0
+    for arr in hint {
+        let hintNumber = arr[0]
+        let strike = arr[1]
+        let ball = arr[2]
+        
+        let hintA = hintNumber / 100
+        let hintB = (hintNumber % 100) / 10
+        let hintC = hintNumber % 10
+        
+        var ballCnt = 0
+        var strikeCnt = 0
+        
+        if hintA == numA {
+            strikeCnt += 1
+        } else if [numB, numC].contains(hintA) {
+            ballCnt += 1
+        }
+        
+        if hintB == numB {
+            strikeCnt += 1
+        } else if [numA, numC].contains(hintB) {
+            ballCnt += 1
+        }
+        
+        if hintC == numC {
+            strikeCnt += 1
+        } else if [numA, numB].contains(hintC) {
+            ballCnt += 1
+        }
+        
+        if ball == ballCnt && strike == strikeCnt {
+            cnt += 1
         }
     }
     
+    if cnt == n {
+        answer += 1
+    }
 }
 
 print(answer)

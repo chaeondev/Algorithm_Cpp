3f971464632f899a75401b19330f7ec3b02c5fb7
Chaewon Yoon <80023607+chaeondev@users.noreply.github.com>
2024-07-11 14:05:25 +0900

    [Bronze III] Title: ACM 호텔, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.swift
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.swift
@@ -4,24 +4,8 @@ let count = Int(readLine()!)!
 
 for _ in 0..<count {
     let input = readLine()!.split(separator: " ").map { Int($0)! }
-    let floorNumber = input[0]
-    let roomNumber = input[1]
-    let guestNumber = input[2]
-
-    var numberY = guestNumber % floorNumber
-    var numberX = 0
-    if numberY == 0 {
-        numberX = guestNumber / floorNumber
-        numberY = floorNumber
-    } else {
-        numberX = guestNumber / floorNumber + 1
-    }
-    
-    if numberX < 10 {
-        let result = String(numberY) + "0" + String(numberX)
-        print(result) 
-    } else {
-        let result = String(numberY) + String(numberX)
-        print(result)
-    }
+    let (H, W, N) = (input[0], input[1], input[2])
+    let floor = N % H == 0 ? H : N % H
+    let room = (N - 1) / H + 1
+    print(floor * 100 + room)
 }
